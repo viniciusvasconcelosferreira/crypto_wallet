a99727981e382c3e95d78c8e50749adb6e432d74
viniciusvasconcelosferreira <viniciusvasconcelos.ferreira@gmail.com>
2021-02-21 03:14:30 -0300

    Alteração da página inicial e teste de rotas personalizadas

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   get 'welcome/index'
   resources :moedas
+  # get '/moedas', to: 'moedas#index'
+  # get '/inicio', to: 'welcome#index'
+  root to: 'welcome#index'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
