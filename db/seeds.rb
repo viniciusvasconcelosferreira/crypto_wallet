ef5987ef50d343e219e8450765ae96ef4aceee7a
Vinicius Vasconcelos Ferreira <vinicius.ferreira@rcstecnologia.com.br>
2021-02-22 12:54:54 -0300

    Criação de rake tasks personalizadas

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Moeda.create!(
+  [
+    {
+      descricao: 'Bitcoin',
+      sigla: 'BTC',
+      url_imagem: 'https://cdn.freebiesupply.com/logos/large/2x/bitcoin-logo-png-transparent.png'
+    },
+    {
+      descricao: 'Ethereum',
+      sigla: 'ETH',
+      url_imagem: 'https://cryptologos.cc/logos/ethereum-eth-logo.png'
+    },
+    {
+      descricao: 'Tether',
+      sigla: 'USDT',
+      url_imagem: 'https://cryptologos.cc/logos/tether-usdt-logo.png'
+    },
+    {
+      descricao: 'Dash',
+      sigla: 'DASH',
+      url_imagem: 'https://cdn.freebiesupply.com/logos/large/2x/dash-3-logo-png-transparent.png'
+    }
+  ]
+)
